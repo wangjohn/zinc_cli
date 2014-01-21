394ef7e475b84f8283565bad9a7d1fd06e027271
wangjohn <wangjohn@mit.edu>
2014-01-20 18:35:46 -0800

    Adding the rest of the responses.
    Still need to work the cases about what happens when you don't put
    certain arguments into the command line.

--- zinc_wizard.py
+++ zinc_wizard.py
@@ -15,7 +15,7 @@ class ValidationHelpers(object):
 class ZincWizard(object):
     PROMPTS = {
         "product_variants": "Welcome to Zinc!\nPlease please enter a product URL.",
-        "select_product_variants": "This item comes in multiple variants. Please choose an option."
+        "select_product_variants": "This item comes in multiple variants. Please choose an option.",
         "select_shipping_methods": "This item has multiple shipping options. Please choose one."
             }
 
@@ -24,9 +24,10 @@ class ZincWizard(object):
 
     def start(self):
         response_data = {}
-        response_data["products"] = self.get_product_variants()
-        response_data["shipping_method_id"] = self.get_shipping_methods(response_data)
-        response_data["store_card_response"] = self.get_store_card(response_data)
+        self.get_product_variants(response_data)
+        self.get_shipping_methods(response_data)
+        self.get_store_card(response_data)
+        self.get_review_order(response_data)
 
     def prompt(self, prompt, validation=None, max_attempts=3):
         attempts = 0
@@ -40,7 +41,7 @@ class ZincWizard(object):
                     print "You've reached the maximum number of attempts. Exiting!"
                     sys.exit()
 
-    def get_product_variants(self):
+    def get_product_variants(self, response_data):
         if "product_url" in self.options:
             product_url = self.options["product_url"]
         else:
@@ -51,30 +52,64 @@ class ZincWizard(object):
                     "retailer": self.options["retailer"],
                     "product_url": product_url
                     })
-        return self.select_product_variants(variants_response)
+        response_data["variant_options_response"] = variants_response
+        response_data["products"] = self.select_product_variants(variants_response)
 
     def get_shipping_methods(self, response_data):
+        shipping_address = self.load_file_contents("shipping_address")
         shipping_response = ZincRequestProcessor.process("shipping_methods", {
                     "client_token": self.options["client_token"],
                     "retailer": self.options["retailer"],
                     "products": response_data["products"],
-                    "shipping_address": self.load_file_contents("shipping_address")
+                    "shipping_address": shipping_address
                 })
 
-        return self.select_shipping_methods(shipping_response)
+        response_data["shipping_response"] = shipping_response
+        response_data["shipping_address"] = shipping_address
+        response_data["shipping_method_id"] = self.select_shipping_methods(shipping_response)
 
     def get_store_card(self, response_data):
         cc_data = self.load_file_contents("credit_card")
         store_card_response = ZincRequestProcessor.process("store_card", {
                     "client_token": self.options["client_token"],
                     "retailer": self.options["retailer"],
-                    "billing_address": self.load_file_contents("billing_address")
+                    "billing_address": self.load_file_contents("billing_address"),
                     "number": cc_data["number"],
                     "expiration_month": cc_data["expiration_month"],
                     "expiration_year": cc_data["expiration_year"]
                 })
 
-        return store_card_response
+        response_data["store_card_response"] = store_card_response
+
+    def get_review_order(self, response_data):
+        payment_method = {
+                "cc_token": response_data["store_card_response"]["cc_token"],
+                "security_code": self.get_security_code()
+                }
+        review_order_response = ZincRequestProcessor.process("review_order", {
+                    "client_token": self.options["client_token"],
+                    "retailer": self.options["retailer"],
+                    "products": response_data["products"],
+                    "shipping_address": response_data["shipping_address"],
+                    "is_gift": self.options["gift"],
+                    "shipping_method_id": response_data["shipping_method_id"],
+                    "payment_method": payment_method,
+                    "customer_email": self.options["email"]
+                })
+
+        response_data["review_order_response"] = review_order_response
+
+    def get_place_order(self, response_data):
+        place_order_resonse = ZincRequestProcessor.process("place_order", {
+                    "client_token": self.options["client_token"],
+                    "place_order_key": response_data["review_order_response"]["place_order_key"]
+                })
+
+        response_data["place_order_response"] = place_order_response
+
+    def get_security_code(self):
+        # FIXME: do it
+        return "123"
 
     def load_file_contents(self, filetype):
         if filetype in self.options:
