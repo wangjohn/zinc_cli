65a58a622af83c89d0683c9cf713b063b07b01cd
wangjohn <wangjohn@mit.edu>
2014-01-23 14:03:44 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ with open('README.rst') as f:
 
 setup(
     name='Zinc',
-    version='0.1.1',
+    version='0.1.2',
     author='John Wang',
     author_email='john@zinc.io',
     packages=['zinc'],
     package_dir={'zinc': ''},
     package_data={'zinc': ['examples/*.py', 'examples/*.json']},
     include_package_data=True,
-    scripts=['bin/zinc_interactive.py','bin/zinc_simple_order.py'],
     url='http://pypi.python.org/pypi/Zinc/',
     license='LICENSE.txt',
     description='Wrapper for Zinc ecommerce API (zinc.io)',
