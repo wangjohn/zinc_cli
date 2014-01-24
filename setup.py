e54cd73f68408e6b37580f04cead1fb70ce6ab4c
wangjohn <wangjohn@mit.edu>
2014-01-24 15:30:24 -0800

    Fixing welcome banner (weclome -> welcome)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = open(README).read() + 'nn'
 
 setup(
     name='Zinc',
-    version='0.1.9',
+    version='0.1.10',
     author='John Wang',
     author_email='john@zinc.io',
     packages=['zinc'],
