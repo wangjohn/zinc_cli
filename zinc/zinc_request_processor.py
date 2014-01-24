5a85d18e0719f721240fc1cbfaea93d60222b2a6
wangjohn <wangjohn@mit.edu>
2014-01-23 21:39:23 -0800

    Allowing for an asynchronous request.

--- zinc/zinc_request_processor.py
+++ zinc/zinc_request_processor.py
@@ -11,6 +11,19 @@ class ZincTimeoutError(Exception):
 class ZincError(Exception):
     pass
 
+class ZincAsyncRequest(object):
+    def __init__(self, url, request_id, abstract_processor, start_time):
+        self.url = url
+        self.request_id = request_id
+        self.abstract_processor = abstract_processor
+        self.start_time = start_time
+
+    def finished(self):
+        self.abstract_processor.finished(self.url, self.request_id)
+
+    def get_response(self):
+        self.abstract_processor.wait_for_response(self.url, self.request_id, self.start_time)
+
 class ZincRequestProcessor(object):
     """Processor for Zinc API requests.
 
@@ -20,6 +33,12 @@ class ZincRequestProcessor(object):
     def process(klass, request_type, payload,
             zinc_base_url="https://api.zinc.io/v0",
             polling_interval=1.0):
+        processor = ZincAbstractProcessor()
+        return processor.post_request(payload, request_type)
+
+    @classmethod
+    def process_async(klass, request_type, payload,
+            zinc_base_url="https://api.zinc.io/v0"):
         processor = ZincAbstractProcessor()
         return processor.post_request(payload, request_type)
 
@@ -43,25 +62,35 @@ class ZincAbstractProcessor(object):
         else:
             return self.zinc_base_url
 
-    def post_request(self, payload, url_stub):
+    def post_request(self, payload, url_stub, async=False):
         start_time = time.time()
         result = requests.post(self.current_url(url_stub), data=json.dumps(payload), timeout=self.post_request_timeout)
         request_id = result.json()["request_id"]
-        return self.wait_for_response(self.current_url(url_stub), request_id, start_time)
+        if async:
+            return ZincAsyncRequest(self.current_url(url_stub), request_id, self, start_time)
+        else:
+            return self.wait_for_response(self.current_url(url_stub), request_id, start_time)
 
-    def wait_for_response(self, url, request_id, start_time):
-        if time.time() - start_time > self.request_timeout:
+    def wait_for_response(self, url, request_id, start_time=None):
+        if start_time != None and time.time() - start_time > self.request_timeout:
             raise ZincTimeoutError("The request '%s' timed out after '%s' seconds" %
                     (request_id, time.time() - start_time))
         time.sleep(self.polling_interval)
+        potential_result = self.finished(url, request_id)
+        if potential_result:
+            end_time = time.time()
+            return potential_result
+        else:
+            return self.wait_for_response(url, request_id, start_time)
+
+    def finished(self, url, request_id):
         result = self.make_request(url + "/" + request_id, self.get_request_retries)
         result_json = result.json()
         if result_json["_type"] == "error" and result_json["code"] == "request_processing":
-            return self.wait_for_response(url, request_id, start_time)
+            return False
         elif result_json["_type"] == "error":
-            raise ZincError(json.dumps(result_json))
+            raise ZincError(result)
         else:
-            end_time = time.time()
             return result_json
 
     def make_request(self, full_url, retries):
